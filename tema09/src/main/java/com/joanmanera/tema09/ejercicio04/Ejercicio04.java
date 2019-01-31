0d359c58a2ba49062c93860a589b3f97062f5096
Joan Manera Clase <joanet31@hotmail.com>
2019-01-31 18:40:16 +0100

    He añadido el ejercicio 04

--- tema09/src/main/java/com/joanmanera/tema09/ejercicio04/Ejercicio04.java
+++ tema09/src/main/java/com/joanmanera/tema09/ejercicio04/Ejercicio04.java
@@ -1,5 +1,6 @@
 package com.joanmanera.tema09.ejercicio04;
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -9,29 +10,34 @@ public class Ejercicio04 {
         int[] array = new int[5];
         try {
             introducirNumerosArray(array);
+        } catch (NullPointerException npe){
+            System.out.println("El array no esta inicializado");
         } catch (IndexOutOfBoundsException iobe){
             System.out.println("El array esta lleno");
-
+            Arrays.toString(array);
         }
 
     }
 
     public void introducirNumerosArray(int[] array){
         Scanner lector = new Scanner(System.in);
+        boolean verificado = false;
 
-        for(int i = 0 ; i < array.length ; i++){
-            try {
-                System.out.print("Introduce un numero: ");
-                array[i] = lector.nextInt();
-                lector.nextLine();
+        for(int i = 0 ; i < 100 ; i++){
+            do{
+                try{
+                    System.out.print("Introduce un numero: ");
+                    array[i] = Integer.parseInt(lector.nextLine());
+                    verificado = true;
+                } catch (InputMismatchException e){
+                    System.out.println("El caracter introducido no es un numero");
+                    verificado = false;
+                } catch (NumberFormatException ne) {
+                    System.out.println("El caracter introducido no es un numero");
+                    verificado = false;
+                }
 
-            } catch (InputMismatchException e){
-                System.out.println("El caracter introducido no es un numero");
-                i = i - 1;
-            } catch (NumberFormatException ne) {
-                System.out.println("El caracter introducido no es un numero");
-                i = i - 1;
-            }
+            } while (!verificado);
         }
     }
 }
