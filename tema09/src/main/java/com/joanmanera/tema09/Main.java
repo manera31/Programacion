c69d30b664d8e4941aa6ec0970b1ebbb1a1794c6
Joan Manera Clase <joanet31@hotmail.com>
2019-01-31 17:04:31 +0100

    mada importante 2.0

---
+++ tema09/src/main/java/com/joanmanera/tema09/Main.java
@@ -0,0 +1,9 @@
+package com.joanmanera.tema09;
+
+import com.joanmanera.tema09.ejercicio01.Ejercicio01;
+
+public class Main {
+    public static void main (String[] args){
+        Ejercicio01 ej01 = new Ejercicio01();
+    }
+}
